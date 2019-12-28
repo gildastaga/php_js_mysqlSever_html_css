6091243108cd3393bcb79d4132d5fd26f6c3461e
1506gitagadoungmomom <1506gitagadoungmomom@PC9728.domepfc.net>
2019-12-28 14:02:53 +0100

    test

---
+++ model/Question.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Question
+ *
+ * @author Alain
+ */
+class Question {
+    //put your code here
+}
