775b2011b8a93d7418b5a009e1bc4193298a3f17
gildas taga <Utilisateur@DESKTOP-8QIBE72.lan>
2020-06-23 18:31:31 +0200

    examen

--- view/view_Stats_questions.php
+++ view/view_Stats_questions.php
@@ -15,7 +15,13 @@ require_once "lib/parsedown-1.7.3/Parsedown.php";
         <link href="lib/jquery-ui-1.12.1.ui-lightness/jquery-ui.structure.min.css" rel="stylesheet" type="text/css"/>
         <script src="lib/jquery-3.4.1.min.js" type="text/javascript"></script>
         <script src="lib/jquery-ui-1.12.1.ui-lightness/jquery-ui.min.js" type="text/javascript"></script>
-        <script src="js/post.js" type="text/javascript"></script>
+        <script >
+        var post= document.querySelector('option');
+        post.onclick=getdetail;
+        function getdetail(post.valueOf()){
+            alert(post.)
+        }
+        </script>
 
 
     </head>
@@ -33,7 +39,7 @@ require_once "lib/parsedown-1.7.3/Parsedown.php";
         <form action="tag/asso_tag_post/<?php echo $posts->PostId; ?>"  method="post">
             <select name="TagId">
                 <?php foreach ($posts as $rows): ?> 
-                        <option value=<?php echo $rows->PostId; ?> ><?php echo $rows->PostId; ?></option>
+                        <option value=<?php echo $rows->PostId; ?> ><?php echo $rows->Title; ?></option>
                 <?php endforeach; ?> 
             </select>        
         </form>
